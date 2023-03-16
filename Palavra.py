9fd4bab08e89b8d83cb21dc1093b45e8d1a59228
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-03-16 10:45:10 -0300

    added funcao randomica gerarPalavra

--- Palavra.py
+++ Palavra.py
@@ -1,10 +1,20 @@
+import json
+import random
+
 class Palavra():
-    def __init__(self, palavra_chave:str):
-        self.palavra_chave = palavra_chave
-    
-    def gerarPalavra(self):
+    def __init__(self):
+        self.palavra_chave = ''
         self.palavra_chave_escondida = ''
-        for letra in self.palavra_chave_escondida:
+    
+    def gerarPalavra(self, dificuldade):
+        arquivo_lista_nomes = open('./utils/lista_palavras.json', "r")
+        lista_nomes = json.loads(arquivo_lista_nomes.read())
+
+        self.palavra_chave = random.choice(lista_nomes[dificuldade])
+
+        arquivo_lista_nomes.close()
+
+        for letra in self.palavra_chave:
             self.palavra_chave_escondida+='_'
 
     def temLetra(self, chute:str):
